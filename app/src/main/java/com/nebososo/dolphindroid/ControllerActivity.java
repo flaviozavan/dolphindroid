a11c33f11eb781265dce79d7ad77dec544fdb826
Flávio Zavan <flaviozavan@gmail.com>
2015-06-13 12:50:10 -0300

    Disconnection after multiple back key presses

--- app/src/main/java/com/nebososo/dolphindroid/ControllerActivity.java
+++ app/src/main/java/com/nebososo/dolphindroid/ControllerActivity.java
@@ -1,16 +1,55 @@
 package com.nebososo.dolphindroid;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 
 public class ControllerActivity extends Activity {
+
+    private long lastBackPress = 0;
+    private Toast backToast;
+    private final int totalBackPresses = 3;
+    private final long maxDelayBetweenBackPresses = 500;
+    private int backPresses = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_controller);
+        backToast = Toast.makeText(getApplicationContext(), null, Toast.LENGTH_SHORT);
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            long currentTime = System.currentTimeMillis();
+
+            if (currentTime - lastBackPress > maxDelayBetweenBackPresses) {
+                backPresses = 1;
+            }
+            else {
+                backPresses++;
+            }
+            lastBackPress = currentTime;
+
+            if (backPresses == totalBackPresses) {
+                backToast.cancel();
+                startActivity(new Intent(this, ConnectionActivity.class));
+                finish();
+            }
+            else {
+
+                backToast.setText((totalBackPresses - backPresses)
+                        + getResources().getString(R.string.presses_to_go));
+                backToast.show();
+            }
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
     }
 }
