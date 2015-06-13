5fb566f04ded3e5b561edd5894b18724b58b1613
Flávio Zavan <flaviozavan@gmail.com>
2015-06-13 12:01:57 -0300

    OK controller layout

--- app/src/main/java/com/nebososo/dolphindroid/ControllerActivity.java
+++ app/src/main/java/com/nebososo/dolphindroid/ControllerActivity.java
@@ -13,27 +13,4 @@ public class ControllerActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_controller);
     }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_controller, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
