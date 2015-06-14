3440b2b79aeb3698cd9a88db2fc111034f140236
Flávio Zavan <flaviozavan@gmail.com>
2015-06-14 13:24:18 -0300

    Connect and send dummy data

--- app/src/main/java/com/nebososo/dolphindroid/ControllerActivity.java
+++ app/src/main/java/com/nebososo/dolphindroid/ControllerActivity.java
@@ -11,6 +11,16 @@ import android.view.MenuItem;
 import android.view.WindowManager;
 import android.widget.Toast;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
 
 public class ControllerActivity extends Activity {
 
@@ -20,6 +30,10 @@ public class ControllerActivity extends Activity {
     private final long maxDelayBetweenBackPresses = 500;
     private int backPresses = 0;
     private PowerManager.WakeLock wl;
+    private DatagramSocket udpSocket;
+    private byte[] sendBuffer = new byte[27];
+    private ScheduledExecutorService sendExecutor = Executors.newSingleThreadScheduledExecutor();
+    private DatagramPacket sendPacket;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +42,47 @@ public class ControllerActivity extends Activity {
         backToast = Toast.makeText(getApplicationContext(), null, Toast.LENGTH_SHORT);
         PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
         wl = pm.newWakeLock(PowerManager.SCREEN_DIM_WAKE_LOCK, "dolphindroid");
+
+        Intent intent = getIntent();
+        String serverAddress = intent.getStringExtra("address");
+        int serverPort = intent.getIntExtra("port", 0);
+
+        try {
+            udpSocket = new DatagramSocket();
+        }
+        catch (SocketException e) {
+            e.printStackTrace();
+        }
+
+        sendBuffer[0] = (byte) 0xde;
+        sendBuffer[1] = (byte) 0x0;
+        /* Accelerometer,buttons and IR */
+        sendBuffer[2] = (byte) 0x7;
+
+        try {
+            sendPacket = new DatagramPacket(sendBuffer, sendBuffer.length,
+                    InetAddress.getByName(serverAddress), serverPort);
+        }
+        catch (UnknownHostException e) {
+            System.out.println(serverAddress);
+            e.printStackTrace();
+        }
+
+        Runnable sendRunnable = new Runnable() {
+            @Override
+            public void run() {
+                sendBuffer[18] = (byte) 0x1;
+
+                try {
+                    udpSocket.send(sendPacket);
+                }
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+
+        sendExecutor.scheduleAtFixedRate(sendRunnable, 50, 50, TimeUnit.MILLISECONDS);
     }
 
     @Override
@@ -40,6 +95,19 @@ public class ControllerActivity extends Activity {
     protected void onPause() {
         wl.release();
         super.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        sendExecutor.shutdown();
+        try {
+            sendExecutor.awaitTermination(1, TimeUnit.SECONDS);
+        }
+        catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        udpSocket.close();
+        super.onDestroy();
     }
 
     @Override
