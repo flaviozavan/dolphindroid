f13da216ba7ae41481dc00de9901e228af1f0ed9
Flávio Zavan <flaviozavan@gmail.com>
2015-06-14 14:15:02 -0300

    Working buttons

--- app/src/main/java/com/nebososo/dolphindroid/ControllerActivity.java
+++ app/src/main/java/com/nebososo/dolphindroid/ControllerActivity.java
@@ -8,7 +8,10 @@ import android.os.PowerManager;
 import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.Toast;
 
 import java.io.IOException;
@@ -17,6 +20,8 @@ import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketException;
 import java.net.UnknownHostException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
@@ -34,6 +39,8 @@ public class ControllerActivity extends Activity {
     private byte[] sendBuffer = new byte[27];
     private ScheduledExecutorService sendExecutor = Executors.newSingleThreadScheduledExecutor();
     private DatagramPacket sendPacket;
+    private AtomicButtonMask buttonMask = new AtomicButtonMask();
+    private Map<Integer, Integer> maskMap = new HashMap<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +49,36 @@ public class ControllerActivity extends Activity {
         backToast = Toast.makeText(getApplicationContext(), null, Toast.LENGTH_SHORT);
         PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
         wl = pm.newWakeLock(PowerManager.SCREEN_DIM_WAKE_LOCK, "dolphindroid");
+
+        maskMap.put(R.id.button_1, 1 << 0);
+        maskMap.put(R.id.button_2, 1 << 1);
+        maskMap.put(R.id.button_a, 1 << 2);
+        maskMap.put(R.id.button_b, 1 << 3);
+        maskMap.put(R.id.button_plus, 1 << 4);
+        maskMap.put(R.id.button_minus, 1 << 5);
+        maskMap.put(R.id.button_home, 1 << 6);
+        maskMap.put(R.id.button_up, 1 << 7);
+        maskMap.put(R.id.button_down, 1 << 8);
+        maskMap.put(R.id.button_left, 1 << 9);
+        maskMap.put(R.id.button_right, 1 << 10);
+        maskMap.put(R.id.button_ul, 1 << 7 | 1 << 9);
+        maskMap.put(R.id.button_ur, 1 << 7 | 1 << 10);
+        maskMap.put(R.id.button_dl, 1 << 8 | 1 << 9);
+        maskMap.put(R.id.button_dr, 1 << 8 | 1 << 10);
+
+        for (Map.Entry<Integer, Integer> entry : maskMap.entrySet()) {
+            Button b = (Button) findViewById(entry.getKey());
+            b.setOnTouchListener(new View.OnTouchListener() {
+                @Override
+                public boolean onTouch(View v, MotionEvent event) {
+                    int action = event.getAction();
+                    if (action == MotionEvent.ACTION_DOWN || action == MotionEvent.ACTION_UP) {
+                        buttonMask.xor(maskMap.get(v.getId()));
+                    }
+                    return true;
+                }
+            });
+        }
 
         Intent intent = getIntent();
         String serverAddress = intent.getStringExtra("address");
@@ -55,7 +92,6 @@ public class ControllerActivity extends Activity {
         }
 
         sendBuffer[0] = (byte) 0xde;
-        sendBuffer[1] = (byte) 0x0;
         /* Accelerometer,buttons and IR */
         sendBuffer[2] = (byte) 0x7;
 
@@ -71,7 +107,12 @@ public class ControllerActivity extends Activity {
         Runnable sendRunnable = new Runnable() {
             @Override
             public void run() {
-                sendBuffer[18] = (byte) 0x1;
+                int mask = buttonMask.get();
+
+                sendBuffer[18] = (byte) (mask & 0xff);
+                sendBuffer[17] = (byte) ((mask >> 8) & 0xff);
+                sendBuffer[16] = (byte) ((mask >> 16) & 0xff);
+                sendBuffer[15] = (byte) ((mask >> 24) & 0xff);
 
                 try {
                     udpSocket.send(sendPacket);
@@ -137,5 +178,17 @@ public class ControllerActivity extends Activity {
             return true;
         }
         return super.onKeyDown(keyCode, event);
+    }
+}
+
+class AtomicButtonMask {
+    private int mask = 0;
+
+    public synchronized int get() {
+        return mask;
+    }
+
+    public synchronized void xor(int v) {
+        mask ^= v;
     }
 }
